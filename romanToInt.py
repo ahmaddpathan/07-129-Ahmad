39813d29ba8d4faa2c0cba8422179cb040130f90
ahmaddpathan <ahmadp@andrew.cmu.edu>
2024-09-14 22:59:07 +0300

    LeetCode Prob 2
    https://leetcode.com/problems/roman-to-integer/description/

---
+++ romanToInt.py
@@ -0,0 +1,25 @@
+# visit https://leetcode.com/problems/roman-to-integer/description/ for problem description
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        rti = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        
+        total = 0
+        length = len(s)
+        
+        for i in range(length):
+            cv = rti[s[i]]
+            if i + 1 < length and cv < rti[s[i + 1]]:
+                total -= cv
+            else:  
+                total += cv
+        return total
+    
+romanToInt("III")
